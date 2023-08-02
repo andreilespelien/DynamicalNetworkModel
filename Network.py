6471ceb41f4ffc1b34f7da08b00b60e26715483a
Andrei Espelien <71854935+andreilespelien@users.noreply.github.com>
2023-08-02 00:31:24 -0400

    3D line attractor

--- Network.py
+++ Network.py
@@ -10,60 +10,74 @@ tVector = np.arange(0, tm, dt)
 Ia = 1
 tau = 0.005
 
-intgr1 = IntgrNeuron("intgr1")
-intgr2 = IntgrNeuron("intgr2")
+ax = plt.figure().add_subplot(projection='3d')
 
-input1 = InputNeuron("input1")
-input1.u[int(tm * 1/5 / dt) : int(tm * 3/5 / dt)] = Ia
-input1.u[int(tm * 3/5 / dt) : int(tm * 5/5 / dt)] = Ia * 1.5
-# input1.u[[i for i in range(int(tm * 1/5 / dt), int(tm * 3/5 / dt), 100)]] = 1
-# input1.u[[i for i in range(int(tm * 3/5 / dt), int(tm * 5/5 / dt), 80)]] = 1
+for i in range(1, 6):
+    intgr1 = IntgrNeuron("intgr1")
+    intgr2 = IntgrNeuron("intgr2")
+    intgr3 = IntgrNeuron("intgr3")
 
-input2 = InputNeuron("input2")
-input2.u[int(tm * 1/5 / dt) : int(tm * 3/5 / dt)] = Ia
-input2.u[int(tm * 3/5 / dt) : int(tm * 5/5 / dt)] = Ia * 0.5
-# input2.u[[i for i in range(int(tm * 1/5 / dt), int(tm * 3/5 / dt), 100)]] = 1
-# input2.u[[i for i in range(int(tm * 3/5 / dt), int(tm * 5/5 / dt), 125)]] = 1
+    input1 = InputNeuron("input1")
+    input1.u[:] = Ia
+    input1.u[int(tm * 0/3 / dt) : int(tm * 1/3 / dt)] = Ia * (1 + i * 0.1)
 
+    input2 = InputNeuron("input2")
+    input2.u[:] = Ia
+    input2.u[int(tm * 1/3 / dt) : int(tm * 2/3 / dt)] = Ia * (1 + i * 0.1)
 
-input1.link(intgr1, 1)
-input1.link(intgr2, 0.99)
-intgr1.link(intgr2, 1.1)
-intgr1.link(intgr1, 0.01)
-# print([(c.n1.name, c.n2.name) for c in intgr1.inputs])
-
-input2.link(intgr2, 1)
-input2.link(intgr1, 0.99)
-intgr2.link(intgr1, 1.1)
-intgr2.link(intgr2, 0.01)
+    input3 = InputNeuron("input3")
+    input3.u[:] = Ia
+    input3.u[int(tm * 2/3 / dt) : int(tm * 3/3 / dt)] = Ia * (1 + i * 0.1)
 
 
-inputs = [input1, input2]
-intgrs = [intgr1, intgr2]
+    input1.link(intgr1, 1)
+    input1.link(intgr3, 0.99)
+    input1.link(intgr2, 0.99)
+    intgr1.link(intgr3, 1.1)
+    intgr1.link(intgr2, 1.1)
+    intgr1.link(intgr1, 0.01)
 
-for t in range(len(tVector) - 1):
-    for currNeuron in intgrs:
-        usum = 0
-        xsum = 0
+    input2.link(intgr2, 1)
+    input2.link(intgr1, 0.99)
+    input2.link(intgr3, 0.99)
+    intgr2.link(intgr1, 1.1)
+    intgr2.link(intgr3, 1.1)
+    intgr2.link(intgr2, 0.01)
 
-        for c in currNeuron.inputs:
-            n = c.n1
-            if type(n) == InputNeuron:
-                usum += n.u[t] * c.w
-            if type(n) == IntgrNeuron:
-                xsum += n.x[t] * c.w
+    input3.link(intgr3, 1)
+    input3.link(intgr2, 0.99)
+    input3.link(intgr1, 0.99)
+    intgr3.link(intgr2, 1.1)
+    intgr3.link(intgr1, 1.1)
+    intgr3.link(intgr3, 0.01)
 
-        dx = ((usum - xsum) - currNeuron.x[t]) / tau * dt
-        currNeuron.dx[t + 1] = dx
-        currNeuron.x[t + 1] = currNeuron.x[t] + dx
 
-fig, ax = plt.subplots(6)
-ax[0].plot(tVector, input1.u)
-ax[1].plot(tVector, intgr1.x)
-# ax[2].plot(tVector, intgr1.dx)
-ax[3].plot(tVector, input2.u)
-ax[4].plot(tVector, intgr2.x)
-# ax[5].plot(tVector, intgr2.dx)
+    inputs = [input1, input2, input3]
+    intgrs = [intgr1, intgr2, intgr3]
 
-# plt.plot(intgr1.x, intgr2.x)
+    for t in range(len(tVector) - 1):
+        for currNeuron in intgrs:
+            usum = 0
+            xsum = 0
+
+            for c in currNeuron.inputs:
+                n = c.n1
+                if type(n) == InputNeuron:
+                    usum += n.u[t] * c.w
+                if type(n) == IntgrNeuron:
+                    xsum += n.x[t] * c.w
+
+            dx = ((usum - xsum) - currNeuron.x[t]) / tau * dt
+            currNeuron.dx[t + 1] = dx
+            currNeuron.x[t + 1] = currNeuron.x[t] + dx
+
+    # fig, ax = plt.subplots(6)
+    # ax[0].plot(tVector, input1.u)
+    # ax[1].plot(tVector, intgr1.x)
+    # ax[2].plot(tVector, input2.u)
+    # ax[3].plot(tVector, intgr2.x)
+    # ax[4].plot(tVector, input3.u)
+    # ax[5].plot(tVector, intgr3.x)
+
+    ax.plot(intgr1.x, intgr2.x, intgr3.x)
 plt.show()
