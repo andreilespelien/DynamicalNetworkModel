7ac3f09d77c852991e9da0698365baa89b2fc474
Andrei Espelien <71854935+andreilespelien@users.noreply.github.com>
2023-08-03 14:47:53 -0400

    Multi-neuron networks and PCA

--- Network.py
+++ Network.py
@@ -1,83 +1,79 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 from Neuron import InputNeuron, IntgrNeuron
-from Connection import Connection
 
-tm = 0.25
-dt = 0.0001
-tVector = np.arange(0, tm, dt)
-Ia = 1
-tau = 0.005
+class Network():
+    tm = 0.1
+    dt = 0.00001
+    tVector = np.arange(0, tm, dt)
+    Ia = 1
+    tau = 0.005
 
-ax = plt.figure().add_subplot(projection='3d')
+    def fWeights(self, x):
+        return 2.5 * (math.exp(-1/2 * (x / 1.01)**2) - math.exp(-1/2 * (x / 0.202)**2))
 
-for i in range(1, 6):
-    intgr1 = IntgrNeuron("intgr1")
-    intgr2 = IntgrNeuron("intgr2")
-    intgr3 = IntgrNeuron("intgr3")
+    def __init__(self, n, spikeRatio):
+        self.n = n
+        self.inputNeurons = []
+        self.intgrNeurons = []
 
-    input1 = InputNeuron("input1")
-    input1.u[:] = Ia
-    input1.u[int(tm * 0/3 / dt) : int(tm * 1/3 / dt)] = Ia * (1 + i * 0.1)
+        for d in range(n):
+            self.intgrNeurons.append(IntgrNeuron("intgr" + str(d)))
+            self.inputNeurons.append(InputNeuron("input" + str(d)))
+            self.inputNeurons[d].u[:] = self.Ia
+            self.inputNeurons[d].u[int(self.tm * d/n / self.dt) : int(self.tm * (d+1)/n / self.dt)] = self.Ia * spikeRatio
 
-    input2 = InputNeuron("input2")
-    input2.u[:] = Ia
-    input2.u[int(tm * 1/3 / dt) : int(tm * 2/3 / dt)] = Ia * (1 + i * 0.1)
+        xWeights = np.linspace(-4, 4, n)
+        yWeights = []
+        for x in xWeights:
+            yWeights.append(self.fWeights(x))
+        # print(yWeights)
+        # plt.plot(xWeights, yWeights)
+        # plt.show()
+        
+        for d in range(n):
+            self.inputNeurons[d].link(self.intgrNeurons[d], 1)
+            self.inputNeurons[d].link(self.intgrNeurons[d - 1], 0.99)
+            self.inputNeurons[d].link(self.intgrNeurons[(d + 1) % n], 0.99)
 
-    input3 = InputNeuron("input3")
-    input3.u[:] = Ia
-    input3.u[int(tm * 2/3 / dt) : int(tm * 3/3 / dt)] = Ia * (1 + i * 0.1)
+            self.intgrNeurons[d].link(self.intgrNeurons[d - 1], 0.99)
+            self.intgrNeurons[d].link(self.intgrNeurons[(d + 1) % n], 0.99)
+            self.intgrNeurons[d].link(self.intgrNeurons[d], 0.01)
+        # print([c.n1.name for c in self.intgrNeurons[0].inputs])
 
+    def run(self):
+        for t in range(len(self.tVector) - 1):
+            for currNeuron in self.intgrNeurons:
+                usum = 0
+                xsum = 0
 
-    input1.link(intgr1, 1)
-    input1.link(intgr3, 0.99)
-    input1.link(intgr2, 0.99)
-    intgr1.link(intgr3, 1.1)
-    intgr1.link(intgr2, 1.1)
-    intgr1.link(intgr1, 0.01)
+                for c in currNeuron.inputs:
+                    n = c.n1
+                    if type(n) == InputNeuron:
+                        usum += n.u[t] * c.w
+                    if type(n) == IntgrNeuron:
+                        xsum += n.x[t] * c.w
 
-    input2.link(intgr2, 1)
-    input2.link(intgr1, 0.99)
-    input2.link(intgr3, 0.99)
-    intgr2.link(intgr1, 1.1)
-    intgr2.link(intgr3, 1.1)
-    intgr2.link(intgr2, 0.01)
+                dx = ((usum - xsum) - currNeuron.x[t]) / self.tau * self.dt
+                currNeuron.dx[t + 1] = dx
+                currNeuron.x[t + 1] = currNeuron.x[t] + dx
 
-    input3.link(intgr3, 1)
-    input3.link(intgr2, 0.99)
-    input3.link(intgr1, 0.99)
-    intgr3.link(intgr2, 1.1)
-    intgr3.link(intgr1, 1.1)
-    intgr3.link(intgr3, 0.01)
+    def plot(self, showDx):
+        if showDx:
+            fig, ax = plt.subplots(self.n * 3)
+            for d in range(self.n):
+                ax[d * 3].plot(self.tVector, self.inputNeurons[d].u)
+                ax[d * 3 + 1].plot(self.tVector, self.intgrNeurons[d].x, label = str(d))
+                ax[d * 3 + 2].plot(self.tVector, self.intgrNeurons[d].dx)
+                ax[d * 3 + 1].legend()
+        else:
+            fig, ax = plt.subplots(self.n * 2)
+            for d in range(self.n):
+                print(d)
+                ax[d * 2].plot(self.tVector, self.inputNeurons[d].u, label = str(d))
+                ax[d * 2 + 1].plot(self.tVector, self.intgrNeurons[d].x)
+                ax[d * 2].legend()
 
-
-    inputs = [input1, input2, input3]
-    intgrs = [intgr1, intgr2, intgr3]
-
-    for t in range(len(tVector) - 1):
-        for currNeuron in intgrs:
-            usum = 0
-            xsum = 0
-
-            for c in currNeuron.inputs:
-                n = c.n1
-                if type(n) == InputNeuron:
-                    usum += n.u[t] * c.w
-                if type(n) == IntgrNeuron:
-                    xsum += n.x[t] * c.w
-
-            dx = ((usum - xsum) - currNeuron.x[t]) / tau * dt
-            currNeuron.dx[t + 1] = dx
-            currNeuron.x[t + 1] = currNeuron.x[t] + dx
-
-    # fig, ax = plt.subplots(6)
-    # ax[0].plot(tVector, input1.u)
-    # ax[1].plot(tVector, intgr1.x)
-    # ax[2].plot(tVector, input2.u)
-    # ax[3].plot(tVector, intgr2.x)
-    # ax[4].plot(tVector, input3.u)
-    # ax[5].plot(tVector, intgr3.x)
-
-    ax.plot(intgr1.x, intgr2.x, intgr3.x)
-plt.show()
+# temp = Network(32, 1.5)
